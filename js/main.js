4c80887479d2572af536c387d6676f54dea8fa17
wjjwkwindy <wjjwkwindy@gmail.com>
2018-12-08 21:52:30 +0800

    Site updated: 2018-12-08 21:52:27

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
-var toggleButton = document.getElementById('g-toggle');
-var navList = document.getElementById('g-navlist');
+var toggleButton = document.getElementById("g-toggle");
+var navList = document.getElementById("g-navlist");
+var now = new Date().getHours();
 
 hljs.initHighlightingOnLoad();
 
+window.addEventListener(
+  "DOMContentLoaded",
+  function() {
+    if (now >= 20 || now <= 6) {
+      toggleDarkMode();
+    }
+  },
+  false
+);
+
 if (toggleButton && navList) {
-    toggleButton.addEventListener('click', function () {
-        navList.classList.toggle('nav-list-show');
-    });
-    window.addEventListener('click', function (event) {
-        if (event.target !== toggleButton) {
-            navList.classList.remove('nav-list-show');
-        }
-    });
+  toggleButton.addEventListener("click", function() {
+    navList.classList.toggle("nav-list-show");
+  });
+  window.addEventListener("click", function(event) {
+    if (event.target !== toggleButton) {
+      navList.classList.remove("nav-list-show");
+    }
+  });
+}
+
+function toggleDarkMode() {
+  var cssLink = document.createElement("link");
+  cssLink.rel = "stylesheet";
+  cssLink.href = "/css/dark.css";
+  document.head.appendChild(cssLink);
 }
