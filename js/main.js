03c59c2f5430702ae50349e65264378e502c200b
wjjwkwindy <wjjwkwindy@gmail.com>
2019-01-09 14:10:35 +0800

    Site updated: 2019-01-09 14:10:33

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var toggleButton = document.getElementById("g-toggle");
-var navList = document.getElementById("g-navlist");
+var toggleButton = document.getElementById('g-toggle');
+var navList = document.getElementById('g-navlist');
 var now = new Date().getHours();
 
 hljs.initHighlightingOnLoad();
 
 window.addEventListener(
-  "DOMContentLoaded",
+  'DOMContentLoaded',
   function() {
     if (now >= 20 || now <= 6) {
       toggleDarkMode();
@@ -15,19 +15,19 @@ window.addEventListener(
 );
 
 if (toggleButton && navList) {
-  toggleButton.addEventListener("click", function() {
-    navList.classList.toggle("nav-list-show");
+  toggleButton.addEventListener('click', function() {
+    navList.classList.toggle('nav-list-show');
   });
-  window.addEventListener("click", function(event) {
+  window.addEventListener('click', function(event) {
     if (event.target !== toggleButton) {
-      navList.classList.remove("nav-list-show");
+      navList.classList.remove('nav-list-show');
     }
   });
 }
 
 function toggleDarkMode() {
-  var cssLink = document.createElement("link");
-  cssLink.rel = "stylesheet";
-  cssLink.href = "/css/dark.css";
+  var cssLink = document.createElement('link');
+  cssLink.rel = 'stylesheet';
+  cssLink.href = '/css/dark.css';
   document.head.appendChild(cssLink);
 }
